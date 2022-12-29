a28b03c27cf27ffb6f15d27f480a6514a0341c07
Chengxi Zang <calvin-zcx@users.noreply.github.com>
2022-12-29 15:33:33 -0500

    simulate code

--- iptw/main_revise_testset_simulate.py
+++ iptw/main_revise_testset_simulate.py
@@ -33,7 +33,7 @@ from PSModels import mlp, lstm, ml
 import itertools
 from tqdm import tqdm
 from sklearn.model_selection import KFold
-import seaborn as sns
+# import seaborn as sns
 
 import functools
 from scipy.stats import bernoulli
@@ -227,11 +227,11 @@ if __name__ == "__main__":
     # plot survival time
     data_debug = pd.DataFrame(
         data={'treatment': z, 't2e': T})
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-    sns.histplot(ax=axes[0], data=data_debug, x='t2e', hue='treatment', kde=True, bins=25, stat="percent", common_norm=False)
-    sns.ecdfplot(ax=axes[1], data=data_debug, x='t2e', hue='treatment', complementary=True)
-    sns.ecdfplot(ax=axes[2], data=data_debug, x='t2e', hue='treatment', complementary=False)
-    plt.show()
+    # fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    # sns.histplot(ax=axes[0], data=data_debug, x='t2e', hue='treatment', kde=True, bins=25, stat="percent", common_norm=False)
+    # sns.ecdfplot(ax=axes[1], data=data_debug, x='t2e', hue='treatment', complementary=True)
+    # sns.ecdfplot(ax=axes[2], data=data_debug, x='t2e', hue='treatment', complementary=False)
+    # plt.show()
 
     # train_ratio = 0.8  # 0.5
     train_ratio = args.train_ratio  # default 0.8
