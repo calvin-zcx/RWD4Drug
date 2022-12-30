814bbe3c0ed92aeb3bf838c5cfd1d03127b763dc
Chengxi Zang <calvin-zcx@users.noreply.github.com>
2022-12-29 21:15:43 -0500

    simulate code

--- iptw/simulate_shell_LR.sh
+++ iptw/simulate_shell_LR.sh
@@ -39,7 +39,7 @@ mkdir -p output/simulate/LR/log
 #python main_revise_testset_simulate.py --nsim 4000 --train_ratio 0.6 --run_model LR --output_dir output/simulate/LR/ --random_seed 7 2>&1 | tee output/simulate/LR/log/simun4000train0.60_S7D267_LR.log
 #python main_revise_testset_simulate.py --nsim 4000 --train_ratio 0.6 --run_model LR --output_dir output/simulate/LR/ --random_seed 8 2>&1 | tee output/simulate/LR/log/simun4000train0.60_S8D267_LR.log
 #python main_revise_testset_simulate.py --nsim 4000 --train_ratio 0.6 --run_model LR --output_dir output/simulate/LR/ --random_seed 9 2>&1 | tee output/simulate/LR/log/simun4000train0.60_S9D267_LR.log
-python main_revise_testset_simulate.py --nsim 6000 --train_ratio 0.8 --run_model LR --output_dir output/simulate/LR/ --random_seed 0 2>&1 | tee output/simulate/LR/log/simun6000train0.80_S0D267_LR.log
+#python main_revise_testset_simulate.py --nsim 6000 --train_ratio 0.8 --run_model LR --output_dir output/simulate/LR/ --random_seed 0 2>&1 | tee output/simulate/LR/log/simun6000train0.80_S0D267_LR.log
 python main_revise_testset_simulate.py --nsim 6000 --train_ratio 0.8 --run_model LR --output_dir output/simulate/LR/ --random_seed 1 2>&1 | tee output/simulate/LR/log/simun6000train0.80_S1D267_LR.log
 python main_revise_testset_simulate.py --nsim 6000 --train_ratio 0.8 --run_model LR --output_dir output/simulate/LR/ --random_seed 2 2>&1 | tee output/simulate/LR/log/simun6000train0.80_S2D267_LR.log
 python main_revise_testset_simulate.py --nsim 6000 --train_ratio 0.8 --run_model LR --output_dir output/simulate/LR/ --random_seed 3 2>&1 | tee output/simulate/LR/log/simun6000train0.80_S3D267_LR.log
